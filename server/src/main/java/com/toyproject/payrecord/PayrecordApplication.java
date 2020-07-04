2484e3f7d6a83c448e92ef9877990d32ba30e7e1
etff <wish2rich@gmail.com>
2020-07-04 11:06:57 +0900

    project init

---
+++ server/src/main/java/com/toyproject/payrecord/PayrecordApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.payrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrecordApplication.class, args);
+    }
+
+}
